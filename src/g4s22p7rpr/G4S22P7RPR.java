f84bf6c8114f5d168aa84aad7e37ab7d5ea5f371
ABIGAILSOFIA <ABIGAILSOFIA@Sofy-Luis>
2021-05-12 21:15:40 -0500

    Practica 7

---
+++ src/g4s22p7rpr/G4S22P7RPR.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s22p7rpr;
+
+/**
+ *
+ * @author ABIGAILSOFIA
+ */
+public class G4S22P7RPR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FrmPrincipal().setVisible (true);
+    }
+}
